17d92f0d36f69c721283707c5b00dd709cb93e96
tim <humbletim@gmail.com>
2011-08-18 13:24:06 -0400

    preliminary __call and __tostring metamethod support; additional runtime type checks (trying to to match lua behavior)

---
+++ tests/fail/indexmeta.lua
@@ -0,0 +1,11 @@
+local t = setmetatable({ name="test" },
+	{ 
+	   __index=function(self, k)
+				  if k == "metaname" then return "fromindex" end
+				  if k == "tabval" then return {1234} end
+			   end,
+	})
+
+assert(t.metaname == "fromindex")
+assert(#t.tabval == 1, #t.tabval) -- <- no works? -t
+assert(t.tabval[1] == 1234)
